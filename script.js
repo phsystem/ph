9201d8410c06d4ca25ee6cfb0a753a6db80d1ebe
phsystem <phsystemdev@outlook.com>
2024-10-18 10:31:16 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 // script.js
 function mostrarPagina(pagina) {
     // Esconde todas as páginas
-    document.getElementById('pagina1').style.display = 'none';
-    document.getElementById('pagina2').style.display = 'none';
+    document.getElementById('sistema').style.display = 'none';
+    document.getElementById('parceiros').style.display = 'none';
+    document.getElementById('geral').style.display = 'none';
 
     // Mostra a página selecionada
     document.getElementById(pagina).style.display = 'flex';
